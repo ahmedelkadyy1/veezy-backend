25087de6c1fcfb50bb225e93f8e8bca714d8ec79
Ahmed Megahed <ahmedmegahedcontact@gmail.com>
2025-04-07 22:21:54 +0300

    Update Video.js

--- models/Video.js
+++ models/Video.js
@@ -50,3 +50,17 @@ module.exports = mongoose.model('Video', VideoSchema);
 //     }
 //   }
 // });
+const mongoose = require('mongoose');
+
+const videoSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  channelName: { type: String, required: true },
+  channelIcon: { type: String, required: true }, // Filename
+  filename: { type: String, required: true },    // Video filename
+  thumbnail: { type: String, required: true },   // Thumbnail filename
+  views: { type: Number, default: 0 },
+  uploadTime: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Video', videoSchema);
